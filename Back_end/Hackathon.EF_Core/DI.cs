83c45fcb201caa6c97c668f2721e5e1b608e54e6
Aleksandr Unhurian <unguryan.sasha@gmail.com>
2023-11-10 14:01:42 +0200

    Init back-end solution

---
+++ Back_end/Hackathon.EF_Core/DI.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Hackathon.EF_Core
+{
+    public static class DI
+    {
+        public static void AddEFCore(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assebmly = Assembly.GetExecutingAssembly();
+
+            //services.AddAutoMapper(assebmly);
+
+            //services.AddDbContext<AuthLinkContext>();
+            
+            //services.AddScoped<IAuthRepository, AuthRepository>();
+        }
+    }
+}
---
+++ Back_end/Hackathon.Infrastructure/DI.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hackathon.Infrastructure
+{
+    public static class DI
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+        }
+    }
+}
