da0be2e605a2bce4109898ddea81ed4875513161
Aleksandr Unhurian <unguryan.sasha@gmail.com>
2023-11-10 20:52:08 +0200

    Changed seed

--- Back_end/Hackathon.Domain/Models/AdminUser.cs
+++ Back_end/Hackathon.Domain/Models/AdminUser.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hackathon.Domain.Models
+﻿namespace Hackathon.Domain.Models
 {
     public class AdminUser
     {
         public int Id { get; set; }
+
         public string UserName { get; set; }
+
         public sbyte UserPassword { get; set; }
     }
 }
--- Back_end/Hackathon.Domain/Models/PointAttribute.cs
+++ Back_end/Hackathon.Domain/Models/PointAttribute.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hackathon.Domain.Models
+﻿namespace Hackathon.Domain.Models
 {
     public class PointAttribute
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
